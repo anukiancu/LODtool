6d017603ac21e83e3df85d952fb0666acec71926
anukiancu <anukiancu@gmail.com>
2021-10-05 20:49:36 +0100

    Added apply_lods function

--- lod_tool/test.py
+++ lod_tool/test.py
@@ -4,20 +4,45 @@ asset_path = "/Game/Meshes"
 static_mesh = unreal.StaticMeshComponent.static_mesh
 
 
-for material_index in range(0, static_mesh.get_num_sections(0)):
-    material_name = static_mesh.get_material(material_index).get_name()
+"""for material_index in range(0, static_mesh.get_num_sections(0)):
+    material_name = static_mesh.get_material(material_index).get_name()"""
 
 
 def check_lods(static_mesh):
     lod_no = unreal.EditorStaticMeshLibrary.get_lod_count(static_mesh)
-    print(static_mesh.get_name())
+    print(f"Mesh name: {static_mesh.get_name()}")
     print(f"current LOD count: {lod_no}")
 
 
+def apply_lods(static_mesh):
+    number_of_verts = unreal.EditorStaticMeshLibrary.get_number_verts(static_mesh, 0)
+    if number_of_verts < 100:
+        return
+    print(f"Applying LODs to mesh {static_mesh.get_name()}")
+    options = unreal.EditorScriptingMeshReductionOptions()
+    options.reduction_settings = [
+        unreal.EditorScriptingMeshReductionSettings(1.0, 1.0),
+        unreal.EditorScriptingMeshReductionSettings(0.8, 0.75),
+        unreal.EditorScriptingMeshReductionSettings(0.4, 0.2),
+    ]
+
+    options.auto_compute_lod_screen_size = False
+    unreal.EditorStaticMeshLibrary.set_lods(static_mesh, options)
+    unreal.EditorAssetLibrary.save_loaded_asset(static_mesh)
+
+    print(f"New LOD count: {unreal.EditorStaticMeshLibrary.get_lod_count(static_mesh)}")
+
+
 def get_material_count(static_mesh):
-    material_count= unreal.DatasmithMeshActorElement.get_material_overrides_count()
-    print(f'Current number of materials on this mesh: {material_count}')
-    return
+    sm_component = unreal.DatasmithMeshActorElement()
+    sm_component.get_mesh_element(static_mesh)
+    material_count = unreal.DatasmithMeshActorElement.get_material_overrides_count(
+        sm_component
+    )
+    return get_material_count
+    """material_count = unreal.DatasmithMeshActorElement.get_material_overrides_count()
+    return"""
+    print(f"Current number of materials on this mesh: {material_count}")
 
 
 """def get_poly_count():
@@ -34,3 +59,5 @@ static_mesh_assets = unreal.EditorFilterLibrary.by_class(
 )
 
 list(map(check_lods, static_mesh_assets))
+list(map(apply_lods, static_mesh_assets))
+list(map(get_material_count, static_mesh_assets))
