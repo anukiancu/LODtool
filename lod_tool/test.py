29aa1376ed7a15408cae73dc9368c2e319b749b7
Ana Iancu <anukiancu@gmail.com>
2021-10-15 12:46:26 +0100

    Added functionality to get bounding box diameter

--- lod_tool/test.py
+++ lod_tool/test.py
@@ -50,6 +50,18 @@ def check_lods(static_mesh):
     print(f"Current number of materials on this mesh: {material_count}")
     return material_count"""
 
+def get_lod_screen_size(static_mesh):
+    lod_screen_size = unreal.EditorStaticMeshLibrary.get_lod_screen_sizes(static_mesh)
+    print(f"LOD screen size: {lod_screen_size}")
+    return lod_screen_size
+
+def get_bounding_box(static_mesh): 
+    datasmith = unreal.DatasmithMeshActorElement()
+    bounding_box = datasmith.get_bounding_box_size()
+
+    print(f"Bounding box size: {bounding_box}")
+    return bounding_box
+
 
 def get_material_count(static_mesh):
     selected_obj = unreal.EditorUtilityLibrary.get_selected_asset_data()[0]
@@ -64,13 +76,13 @@ def get_material_count(static_mesh):
     return num_of_materials
 
 
-def get_asset_triangle_count(static_mesh):
+'''def get_asset_triangle_count(static_mesh):
     selected_obj = unreal.EditorUtilityLibrary.get_selected_asset_data()[0]
     selected_actor = selected_obj.get_asset()
     poly_count = unreal.EditableMesh.get_polygon_count(selected_actor)
 
     print(f"Number of polys for this mesh: {poly_count}")
-    return poly_count
+    return poly_count'''
 
 
 if __name__ == "__main__":
@@ -91,13 +103,17 @@ if __name__ == "__main__":
     for mesh in static_mesh_assets:
         mesh_info = {}
         mesh_name = get_mesh_name(mesh)
+        bounding_box = get_bounding_box(mesh)
+        lod_screen_size = get_lod_screen_size(mesh)
         lod_count = check_lods(mesh)
         material_count = get_material_count(mesh)
-        poly_count = get_asset_triangle_count(mesh)
+        #poly_count = get_asset_triangle_count(mesh)
         mesh_info["mesh_name"] = mesh_name
+        mesh_info["bounding_box"] = bounding_box
         mesh_info["material_count"] = material_count
         mesh_info["lod_count"] = lod_count
-        mesh_info["polygon_count"] = poly_count
+        mesh_info["LOD_screen_size"] = lod_screen_size
+        #mesh_info["polygon_count"] = poly_count
         mesh_properties.append(mesh_info)
 
     keys = mesh_properties[0].keys()
