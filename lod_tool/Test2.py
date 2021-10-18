927cc7853e21ee98ee7903f63446b594baa0f55d
anukiancu <anukiancu@gmail.com>
2021-10-18 21:16:34 +0100

    Finally got triangle count functionality in this bitch

--- lod_tool/Test2.py
+++ lod_tool/Test2.py
@@ -1,6 +1,5 @@
 import unreal
 import csv
-import pathlib
 from pathlib import Path
 
 asset_path = "/Game/Meshes"
@@ -41,56 +40,60 @@ def get_simple_collision_count(obj):
     return collision_count
 
 
-"""def get_lod_info(obj):
-    build_settings = unreal.EditorStaticMeshLibrary.get_lod_build_settings(obj, 1)
-    print(f"LOD build settings: {build_settings}")
-    return build_settings"""
-
-
-"""def get_polygon_count(obj):
-    editable_mesh = unreal.EditableMeshFactory.make_editable_mesh(obj, 0)
-    poly_count = unreal.EditableMesh.get_polygon_count(editable_mesh)
-    print(f"Polygon count for this mesh: {poly_count}")
-    return poly_count"""
-
-
 def get_tri_count(obj):
-    inst = unreal.MeshDescriptionBase()
+    inst = obj.create_static_mesh_description()
     poly_group = inst.create_polygon_group()
     polygons = inst.get_polygon_group_polygons(poly_group)
     num_of_polygons = inst.get_num_polygon_group_polygons(poly_group)
     tri_list = []
 
     for poly in polygons:
-        triangles = unreal.MeshDescriptionBase.get_num_polygon_triangles(poly)
+        triangles = inst.get_num_polygon_triangles(poly)
         tri_list.append(triangles)
 
     total_tris = num_of_polygons * sum(tri_list)
     print(f"Number of tris for this mesh: {total_tris}")
     return total_tris
 
+
+def get_lod_triangles(static_mesh):
+    lod_details = []
+
+    lod_no = unreal.EditorStaticMeshLibrary.get_lod_count(static_mesh)
+
+    for lod in range(lod_no):
+        triangle_count = 0
+        num_sections = static_mesh.get_num_sections(lod)
+        for section_number in range(num_sections):
+            section_data = unreal.ProceduralMeshLibrary.get_section_from_static_mesh(
+                static_mesh, lod, section_number
+            )
+            triangle_count += len(section_data[1])
+        lod_detail_dict = {"lod_number": lod, "triangle_count": triangle_count / 3}
+        lod_details.append(lod_detail_dict)
+
+    for lod_detail_dict in lod_details:
+        print(
+            f"LOD number {lod_detail_dict.get('lod_number')} has {lod_detail_dict.get('triangle_count')} triangles.\n"
+        )
+    return lod_details
+
+
 def get_lod_screen_size(obj):
     lod_screen_size = unreal.EditorStaticMeshLibrary.get_lod_screen_sizes(obj)
     print(f"LOD screen size: {lod_screen_size}")
     return lod_screen_size
 
-def get_tris(obj):
-    mesh_factory = unreal.EditableMeshFactory()
-    editable_mesh = unreal.EditableMesh()
-    make_editable_mesh = mesh_factory.make_editable_mesh(obj, 0)
-    # poly_group = instt.create_polygon_groups(polygon_group_attributes = [[]], original_polygon_group_id=[])
-    poly_count = editable_mesh.get_polygon_count(make_editable_mesh)
-    print(f"Polygon count: {poly_count}")
-    return poly_count
+
+def get_bounding_box(obj):
+    bounding_box = obj.get_bounding_box()
+
+    print(f"Bounding box: {bounding_box.max}")
+    print(f"Bounding box: {bounding_box.min}")
+    return bounding_box
 
 
 selected_assets = get_selected_asset()
-
-"""for asset in selected_assets:
-    #asset_name = get_mesh_name(asset)
-    num_of_mats = get_num_mesh_materials(asset)
-    #lod_no = check_lods(asset)
-    print(f"Current number of materials: {num_of_mats}")"""
 
 
 if __name__ == "__main__":
@@ -104,17 +107,22 @@ if __name__ == "__main__":
         material_count = get_num_mesh_materials(assets)
         simple_collission_count = get_simple_collision_count(assets)
         lod_screen_size = get_lod_screen_size(assets)
-        #poly_count = get_tris(assets)
+        bounding_box = get_bounding_box(assets)
+        # poly_count = get_tris(assets)
         # lod_settings = get_lod_info(assets)
         # tri_count = get_tri_count(assets)
         # poly_count = get_polygon_count(assets)
         # poly_count = get_asset_triangle_count(mesh)
+        fucking_hell = get_lod_triangles(assets)
         mesh_info["mesh_name"] = mesh_name
         # mesh_info["bounding_box"] = bounding_box
         mesh_info["material_count"] = material_count
         mesh_info["lod_count"] = lod_count
         mesh_info["simple_collisions"] = simple_collission_count
-        mesh_info['LOD_screen_size'] = lod_screen_size
+        mesh_info["LOD_screen_size"] = lod_screen_size
+        mesh_info["LOD_triangles"] = fucking_hell
+        mesh_info["box_min"] = bounding_box.min
+        mesh_info["box_max"] = bounding_box.max
         # mesh_info["LOD_settings"] = lod_settings
         # mesh_info["tri_count"] = tri_count
         # mesh_info["polygon_count"] = poly_count
@@ -124,7 +132,7 @@ if __name__ == "__main__":
 
     keys = mesh_properties[0].keys()
 
-    path = Path("H:/LODtool/Unreal/LOD_tryout/Content")
+    path = Path("I:/WIPs/LOD_tool_project/LODtool/Content")
     path.mkdir(parents=True, exist_ok=True)
 
     file_path = (path / "meshProperties").with_suffix(".csv")
