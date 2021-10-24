d21db07f4566ba8b51735d69798542395c67af18
anukiancu <anukiancu@gmail.com>
2021-10-24 11:04:33 +0100

    Changed log error message in asset_renaming.py

--- lod_tool/asset_renaming.py
+++ lod_tool/asset_renaming.py
@@ -1,35 +1,34 @@
 import unreal
 
-sel_assets = unreal.EditorUtilityLibrary.get_selected_assets()
+selected_assets = unreal.EditorUtilityLibrary.get_selected_assets()
 
 
-for sa in sel_assets:    
-    asset_name = sa.get_name()    
+for sa in selected_assets:
+    asset_name = sa.get_name()
     asset_folder = unreal.Paths.get_path(sa.get_path_name())
-    #Check if this asset is a Texture. Then check if it's name starts with T_. If not, prepend T_ to the name.
     if sa.get_class().get_name() == "Texture2D":
         if not asset_name.startswith("T_"):
-            if not unreal.EditorAssetLibrary.rename_asset(sa.get_path_name(), asset_folder + "/T_" + asset_name):
-                unreal.log_error("Could not rename: " + sa.get_path_name())
+            if not unreal.EditorAssetLibrary.rename_asset(
+                sa.get_path_name(), asset_folder + "/T_" + asset_name
+            ):
+                unreal.log_error(
+                    "Texture asset already named properly" + sa.get_path_name()
+                )
 
     elif sa.get_class().get_name() == "Material":
         if not asset_name.startswith("M_"):
-            if not unreal.EditorAssetLibrary.rename_asset(sa.get_path_name(), asset_folder + "/M_" + asset_name):
-                unreal.log_error('Could not rename' + sa.get_path_name())
+            if not unreal.EditorAssetLibrary.rename_asset(
+                sa.get_path_name(), asset_folder + "/M_" + asset_name
+            ):
+                unreal.log_error(
+                    "Material asset already named properly" + sa.get_path_name()
+                )
 
     elif sa.get_class().get_name() == "StaticMesh":
         if not asset_name.startswith("SM_"):
-            if not unreal.EditorAssetLibrary.rename_asset(sa.get_path_name(), asset_folder + "/SM_" + asset_name):
-                unreal.log_error("Could not rename" + sa.get_path_name())
-
-import unreal
-
-
-sel_assets = unreal.EditorUtilityLibrary.get_selected_assets()
-for sa in sel_assets:    
-    asset_name = sa.get_name()    
-    asset_folder = unreal.Paths.get_path(sa.get_path_name())
-    if sa.get_class().get_name() == "Material":
-        if not asset_name.startswith("M_"):
-            if not unreal.EditorAssetLibrary.rename_asset(sa.get_path_name(), asset_folder + "/M_" + asset_name):
-                print("Material already named properly" + sa.get_path_name())
+            if not unreal.EditorAssetLibrary.rename_asset(
+                sa.get_path_name(), asset_folder + "/SM_" + asset_name
+            ):
+                unreal.log_error(
+                    "Static mesh asset already named properly" + sa.get_path_name()
+                )
